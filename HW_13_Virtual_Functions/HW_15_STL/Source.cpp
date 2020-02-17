e4fe74f7a668d22e17ab699ed8c2af538ccb262e
Алексей Малоиван <vergilium2@gmail.com>
2020-02-17 10:26:44 +0200

    hw 15 cpp

---
+++ HW_13_Virtual_Functions/HW_15_STL/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class calc {
+
+};
+
+int main() {
+
+	string inpExpr;
+
+	cout << "Enter new expression!" << endl;
+	getline(cin, inpExpr);
+
+	return 0;
+}
