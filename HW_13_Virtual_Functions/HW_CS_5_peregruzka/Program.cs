2833179a50936d7da0109fcd9c088e55f1b2023b
Алексей Малоиван <vergilium2@gmail.com>
2020-04-11 11:32:54 +0300

    HW_CS_5_start

---
+++ HW_13_Virtual_Functions/HW_CS_5_peregruzka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_CS_5_peregruzka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
