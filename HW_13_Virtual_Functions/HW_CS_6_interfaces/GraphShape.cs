a6b7c03d954460f0484ecc78a13f60e58dc5b0ca
Алексей Малоиван <vergilium2@gmail.com>
2020-04-18 09:58:23 +0300

    CS HW 6 task 2 processing

--- HW_13_Virtual_Functions/HW_CS_6_interfaces/GraphShape.cs
+++ HW_13_Virtual_Functions/HW_CS_6_interfaces/GraphShape.cs
@@ -7,12 +7,68 @@ using System.Drawing;
 
 namespace HW_CS_6_interfaces {
 	namespace Task2 {
-		//interface IShape {
-		//	char _body { get; set; }
-		//	Point 
-		//	void Drow();
+		interface IDrawable {
+			void Draw(char с = '*');
+		}
+		abstract class Shape {
+			public string m_sName { get; private set; }
+			public ConsoleColor m_cColor { get; set; }
+			private Point m_pStartPosition { get; set; }
+			public Shape(string name, ConsoleColor color, Point stPos) {
+				m_sName = name;
+				m_cColor = color;
+				m_pStartPosition = stPos;
+			}
+			public Shape(string name, ConsoleColor color, int x, int y) {
+				m_sName = name;
+				m_cColor = color;
+				Move(x, y);
+			}
+			public virtual void Move(Point pos) {
+				m_pStartPosition = pos;
+			}
+			public virtual void Move(int x, int y) {
+				Point xy = new Point(x, y);
+				m_pStartPosition = xy;
+			}
+		}
 
-		//}
+		class Triangle : Shape, IDrawable {
+
+			private int m_iWidth;
+			private int m_iHigth;
+			private int Width{ 
+				get => m_iWidth; 
+				set {
+					if(value >= 3) m_iWidth = value;
+					else throw new System.ArgumentException("Min value is '3'");
+				}
+			}
+			private int Higth{ 
+				get=>m_iHigth;
+				set {
+					if(value >= 2) m_iHigth = value;
+					else throw new System.ArgumentException("Min value is '2'");
+				}
+			}
+			public Triangle(int w, int h, int x = 0, int y = 0, ConsoleColor color = ConsoleColor.White) : base("Triangle", color, x, y) {
+				Width = w;
+				m_iHigth = h;
+			}
+
+			public void Draw(char с) {
+
+				for(int i = 0; i < m_iHigth; i++) {
+					for(int j = i; j < m_iWidth/2-1; j++) {
+						Console.Write(" ");
+					}
+					for(int j = m_iWidth / 2-i; j < m_iWidth; j++) {
+						Console.Write("*");
+					}
+					Console.WriteLine();
+				}
+			}
+		}
 
 	}
 	
--- HW_13_Virtual_Functions/HW_CS_6_interfaces/Program.cs
+++ HW_13_Virtual_Functions/HW_CS_6_interfaces/Program.cs
@@ -12,7 +12,7 @@ namespace HW_CS_6_interfaces {
 	class Program {
 		static void Main(string[] args) {
 			IPolygon[] pol = {
-				new Triangle(10, 10, 20),
+				new Triangle(10, 10, 10),
 				new Square(25),
 				new Rhombus(20, 10),
 				new Rectungle(15,30),
@@ -20,18 +20,19 @@ namespace HW_CS_6_interfaces {
 				new Trapezoid(30, 10, 10),
 				new Cicle(12),
 				new Elips(12,6)
-		};
+			};
 
 			Composition obj = new Composition();
 			for(int i = 0; i < pol.Length; ++i) {
 				obj.addShape(pol[i]);
 			}
 
-			
 
-
+			Task2.IDrawable[] objs = {
+				new Task2.Triangle(10,10,0,0,ConsoleColor.Red)
+			};
+			objs[0].Draw();
 			Console.ReadKey();
-			
 		}
 	}
 }
