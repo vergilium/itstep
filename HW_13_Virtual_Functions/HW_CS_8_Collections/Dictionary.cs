342d3fdf11062b129b85a5cd865bbb5efe8fcf31
Алексей Малоиван <vergilium2@gmail.com>
2020-04-19 18:53:28 +0300

    CS 8 start

---
+++ HW_13_Virtual_Functions/HW_CS_8_Collections/Dictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_CS_8_Collections
+{
+	class Dictionary{
+		private Dictionary<string, string> m_RUEN;
+		private Dictionary<string, string> m_ENRU;
+		public int language { 
+			get => language;
+			set {
+				if (language < 0 && language > 1)
+					throw new System.ArgumentException("Language must be in the range between 0 and 1");
+				else language = value;
+			} 
+		}
+
+		public Dictionary(int lang = 0) {
+			m_RUEN = new Dictionary<string, string>();
+			m_ENRU = new Dictionary<string, string>();
+			language = lang;
+		}
+
+		public void AddWord(string key, string value) {
+			m_lpDict.Add(key, value);
+		}
+		public string GetValue(string key) {
+			return m_lpDict.GetValueOrDefault(key);
+		}
+
+		public string GetKey(string value) {
+			return m_lpDict.
+		}
+	}
+}
---
+++ HW_13_Virtual_Functions/HW_CS_8_Collections/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HW_CS_8_Collections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
