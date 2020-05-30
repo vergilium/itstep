35da5a434b1b508c437758a2ad49916e9cd2784e
Vergilium <vergilium2@gmail.com>
2020-05-30 17:55:29 +0300

    HW CS WF 3 Phonebook start

---
+++ ITStepWorks/HW_CS_WF_3_Phonebook/XML_DAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace HW_CS_WF_3_Phonebook {
+    class XML_DAL {
+        XmlDocument xmlDoc;
+        public XML_DAL() :
+            this("phonebook.xml") { }
+
+        public XML_DAL(string path) {
+            try {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(path);
+            } catch (FileNotFoundException) {
+                MessageBox.Show("File is not found!");
+            } finally {
+
+            }
+        }
+
+        public bool SaveFile(ref object odj) {
+            return true;
+        }
+
+        public bool OpenFile(out object obj) {
+            obj = new object();
+            return true;
+        }
+        
+
+    }
+}
