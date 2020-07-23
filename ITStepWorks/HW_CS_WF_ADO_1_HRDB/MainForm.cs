0aa1614a8f6928bd1ac36ea7603a93f0542330d6
vergilium <vergilium2@gmail.com>
2020-07-23 10:16:30 +0300

    HW CS ADO starting

---
+++ ITStepWorks/HW_CS_WF_ADO_1_HRDB/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_CS_WF_ADO_1_HRDB {
+	public partial class MainForm : Form {
+		SqlConnection _conn;
+		DbCommand sqlCmd;
+		public MainForm(SqlConnection conn) {
+			InitializeComponent();
+			_conn = conn;
+		}
+
+		private void MainForm_Load(object sender, EventArgs e) {
+			try {
+				if(_conn.State != ConnectionState.Open) _conn.Open();
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message, "Err connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Application.Exit();
+			}
+			sqlCmd = new SqlCommand("exec selectEmployees null, null, 'alex'", _conn);
+			dataGrid.DataSource = sqlCmd.ExecuteReader(); 
+			
+		}
+
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+			_conn.Close();
+		}
+	}
+}
---
+++ ITStepWorks/HW_CS_WF_ADO_1_HRDB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_CS_WF_ADO_1_HRDB {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() {
+			string connString = ConfigurationManager.ConnectionStrings["HRresurceConnectionString"].ConnectionString;
+			SqlConnection conn = new SqlConnection(connString);
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm(conn));
+
+		}
+	}
+}
